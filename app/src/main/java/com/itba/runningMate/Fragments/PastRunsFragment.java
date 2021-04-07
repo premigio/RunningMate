b28cc29e78f7ff72f9bbb16e7199a3af16f21dc9
pedroremigiopingarilho <premigio@itba.edu.ar>
2021-04-07 16:38:03 -0300

    tab menu, not perfect

---
+++ app/src/main/java/com/itba/runningMate/Fragments/PastRunsFragment.java
@@ -0,0 +1,30 @@
+package com.itba.runningMate.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.itba.runningMate.R;
+
+public class PastRunsFragment extends Fragment {
+
+    public PastRunsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_past_runs, container, false);
+    }
+}
--- app/src/main/java/com/itba/runningMate/MainActivity.java
+++ app/src/main/java/com/itba/runningMate/MainActivity.java
@@ -1,16 +1,36 @@
 package com.itba.runningMate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.view.WindowManager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.itba.runningMate.Fragments.Adapters.ViewPagerAdapter;
+import com.itba.runningMate.Fragments.PastRunsFragment;
+import com.itba.runningMate.Fragments.RunFragment;
+
+import static androidx.fragment.app.FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tab_menu_layout);
+        setUpTabs();
+    }
+
+    private void setUpTabs() {
+        ViewPagerAdapter adapter =
+                new ViewPagerAdapter(getSupportFragmentManager(),BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        adapter.addFragment(getString(R.string.run) , new RunFragment());
+        adapter.addFragment(getString(R.string.past) , new PastRunsFragment());
+        ViewPager vp = findViewById(R.id.viewPager);
+        vp.setAdapter(adapter);
+        TabLayout tb = findViewById(R.id.tabLayout);
+        tb.setupWithViewPager(vp);
 
     }
 }
